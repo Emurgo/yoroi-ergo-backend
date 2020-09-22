065540475981112fdc0ffcc7a6e9bbceb7ae838b
Marek Mahut <marek.mahut@gmail.com>
2020-09-22 17:03:38 +0200

    default.nix: refactor

--- default.nix
+++ default.nix
@@ -1,52 +1,24 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9480bae337095fd24f61380bce3174fdfe926a00.tar.gz") {}
+}:
 with pkgs;
-
 let
   packageJSON = builtins.fromJSON (builtins.readFile ./package.json);
 
-  npm-to-nix = fetchFromGitHub {
+  nix-npm-buildpackage = pkgs.fetchFromGitHub {
     owner = "Emurgo";
-    repo = "npm-to-nix";
-    rev = "6d2cbbc9d58566513019ae176bab7c2aeb68efae";
-    sha256 = "1wm9f2j8zckqbp1w7rqnbvr8wh6n072vyyzk69sa6756y24sni9a";
+    repo = "nix-npm-buildpackage";
+    rev = "abde678d1584af0ad00477486bca26c880963a70";
+    sha256 = "sha256-apHZDERTGe+kdAPVnFltIZvtoMvuqjpm5lqpII+ZfHc=";
   };
-
 in rec {
+  inherit (pkgs.callPackage nix-npm-buildpackage {}) buildNpmPackage;
 
-  inherit (callPackage npm-to-nix {}) npmToNix;
-
-  yoroi-ergo-backend = stdenv.mkDerivation rec {
-    pname = "yoroi-ergo-backend";
-    version = packageJSON.version;
+  yoroi-ergo-backend = buildNpmPackage {
     src = ./.;
-    buildInputs = [ nodejs nodePackages.typescript ];
 
-    preConfigure = ''
-      cp -r ${npmToNix { inherit src; }} node_modules
-      chmod -R +w node_modules
-      patchShebangs node_modules
-    '';
+    npmBuild = "npm run _flow-remove-types";
+    postInstall = "cp -r flow-removed $out";
 
-    buildPhase = ''
-      export HOME=$TMPDIR
-      npm install
-      npm run _flow-remove-types
-    '';
-
-    installPhase = ''
-      mkdir $out
-      mv * $out
-     
-      mkdir -p $out/bin
-      cat <<EOF > $out/bin/yoroi-ergo-backend
-      #!${runtimeShell}
-      exec ${nodejs}/bin/node $out/flow-removed/index.js
-      EOF
-      chmod +x $out/bin/yoroi-ergo-backend
-    '';
-
-    fixupPhase = ''
-      patchShebangs $out
-    '';
   };
 }
